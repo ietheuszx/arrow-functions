5ff0a238213fd7d3f6d9e83c95bc58a792a7ae53
ietheuszx <fernandomaranss68@gmail.com>
2024-04-24 16:53:22 -0400

    adicionado questão4

---
+++ questao4.js
@@ -0,0 +1,13 @@
+/*
+Questão 04: Escreva uma arrow function chamada ehPar que recebe um número 
+como parâmetro e retorna true se o número for par e false se for ímpar.
+*/
+
+const ehPar = (x) => {
+    if(x % 2==0) {
+        return "true"
+    } else {
+        return "false"
+    };
+};
+console.log(ehPar(2))

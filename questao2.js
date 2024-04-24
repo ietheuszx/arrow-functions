be31f07ecc1aaf96ce4426e718d50d6ec8624d94
ietheuszx <fernandomaranss68@gmail.com>
2024-04-24 16:53:10 -0400

    adicionado questão2

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome 
+como parâmetro e retorna uma string de saudação
+*/
+let nome = "Olá, Veadão!"
+const saudacao = (nome) => {
+    return nome = "Olá, Veadão!" 
+}
+console.log(nome)
